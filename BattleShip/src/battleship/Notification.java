3187ef7d2a93eb8e4bdec201cd131720f0742270
Eryk <Eryk@Eryk-PC>
2015-11-16 23:41:32 +0100

    Creating class BattleShip with main function, BattleFrame which is a JFrame form with some of its components

---
+++ BattleShip/src/battleship/Notification.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Eryk
+ */
+public class Notification extends JLabel {
+    
+}
---
+++ BattleShip/src/battleship/PlayAgain.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Eryk
+ */
+public class PlayAgain extends JButton {
+    
+}
